30ebd857bec6f316aed282c00b05ce7a16c501d5
Eunyoung Chang <eunyoung@taejae.ac.kr>
2024-01-19 17:58:57 +0900

    Eunyoung's Calculator 진화

--- Eunyoung's Calculator.py
+++ Eunyoung's Calculator.py
@@ -1,20 +1,32 @@
 print("Welcome to Eunyonng's Calculator!\n")
-print("Please slect and enter an operation:")
-print("+\n""-\n""*\n" "/\n")
-selection = input()
-print("You selected " + selection + "\n")
-x = int(input("Please enter the first number: "))
-y = int(input("Please enter the second number: "))
-print("")
-if (selection == '+'):
-  print (x, '+', y, '=', x+y)
-elif (selection == '-'):
-  print(x, '-', y, '=', x-y)
-elif (selection == '*'):
-  print(x, '*', y, '=', x*y)
-elif (selection == '/'):
-  print(x, '/', y, '=', x/y)
-else:
-  print('ERROR!')
-print("")
+
+while True:
+    print("Please select and enter an operation:")
+    print("+\n-\n*\n/\n")
+    selection = input()
+    print("You selected " + selection + "\n")
+
+    x = int(input("Please enter the first number: "))
+    y = int(input("Please enter the second number: "))
+    print("")
+
+    if selection == '+':
+        print(x, '+', y, '=', x + y)
+    elif selection == '-':
+        print(x, '-', y, '=', x - y)
+    elif selection == '*':
+        print(x, '*', y, '=', x * y)
+    elif selection == '/':
+        print(x, '/', y, '=', x / y)
+    else:
+        print('ERROR!')
+
+    print("")
+
+    another = input("Is there anything else you would like to do? Enter yes or no: ")
+    if another == 'yes':
+        continue
+    elif another == 'no':
+        break
+
 print("Thank you for using Eunyoung's Calculator!")
